564c923e021c5795e2d836d416aa16232fb5483a
NarixenoAdaxius <dauis.arionejohn@gmail.com>
2025-02-25 03:33:22 +0800

    Logger for Chatbot Debug

--- AIModel.vb
+++ AIModel.vb
@@ -4,18 +4,25 @@ Imports Newtonsoft.Json.Linq
 Public Class AIModel
     Public Shared Function GetAIResponse(prompt As String) As String
         Try
+            ' Create REST client
             Dim client As New RestClient("http://localhost:11434/api/generate")
             Dim request As New RestRequest(Method.Post)
 
+            ' Add JSON request body
             request.AddJsonBody(New With {
                 .model = "llama3",
                 .prompt = prompt,
                 .stream = False
             })
 
+            ' Execute request and get response
             Dim response As RestResponse = client.Execute(request)
 
-            ' Check if the response is valid
+            ' Log response to debug
+            Console.WriteLine("Response Status: " & response.StatusCode)
+            Console.WriteLine("Response Content: " & response.Content)
+
+            ' Check if response is valid
             If response.StatusCode = System.Net.HttpStatusCode.OK Then
                 Dim jsonResponse As JObject = JObject.Parse(response.Content)
                 Return jsonResponse("response").ToString()
